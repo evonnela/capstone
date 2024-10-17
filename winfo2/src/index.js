a476c3d821fccda47d9cc6814b46ce0a464ab151
selenayu000 <selenayu2003@gmail.com>
2024-10-17 13:04:53 -0700

    firebase

--- winfo2/src/index.js
+++ winfo2/src/index.js
@@ -4,6 +4,27 @@ import './index.css';
 import App from './App';
 
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5I6ATgbK8v4GjtJQxyyH8nVpdTVBfZJU",
+  authDomain: "winfo20-6773f.firebaseapp.com",
+  projectId: "winfo20-6773f",
+  storageBucket: "winfo20-6773f.appspot.com",
+  messagingSenderId: "899556997428",
+  appId: "1:899556997428:web:7248d05802df22b4d30d61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+import { getDatabase } from 'firebase/database';
+const db = getDatabase();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
