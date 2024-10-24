f66135305989dc5d2aa8ac44eed7475684e4cac5
Jenna Sorror <jsorror@uw.edu>
2024-10-24 13:07:57 -0700

    routers

--- winfo2/src/App.js
+++ winfo2/src/App.js
@@ -9,3 +9,27 @@ import NavBar from './components/NavBar/NavBar.js';
 import Profile from './components/Profile/Profile.js';
 import ProgressBar from './components/ProgressBar/ProgressBar.js';
 import Quiz from './components/Quiz/Quiz.js';
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+
+export default function App(props) {
+    return (
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/AccessoriesFilter" element={<AccessoriesFilter />} />
+            <Route path="/Book" element={<Book/>} />
+            <Route path="/BookFilter" element={<BookFilter/>} />
+            <Route path="/CharacterBuilding" element={<CharacterBuilding/>} />
+            <Route path="/Home" element={<Home/>} />
+            <Route path="/Inventory" element={<Inventory/>} />
+            <Route path="/MarketPlace" element={<MarketPlace/>} />
+            <Route path="/NavBar" element={<NavBar/>} />
+            <Route path="/Profile" element={<Profile/>} />
+            <Route path="/ProgressBar" element={<ProgressBar/>} />
+            <Route path="/Quiz" element={<Quiz/>} />
+          </Routes>
+        </Router> 
+      </div>
+    );
+  }
+  
