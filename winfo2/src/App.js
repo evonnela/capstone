dd05a4ba07380b475c0da09453250739e2cfeb3a
Jenna Sorror <jsorror@uw.edu>
2024-11-01 11:45:40 -0700

    app

--- winfo2/src/App.js
+++ winfo2/src/App.js
@@ -1,14 +1,14 @@
-import AccessoriesFilter from './components/AccessoriesFilter/AccessoriesFilter.js';
-import Book from './components/Book/Book.js';
-import BookFilter from './components/BookFilter/BookFilter.js';
-import CharacterBuilding from './components/CharacterBuilding/CharacterBuilding.js';
-import Home from './components/Home/Home.js';
-import Inventory from './components/Invnetory/Inventory.js';
-import MarketPlace from './components/MarketPlace/MarketPlace.js';
-import NavBar from './components/NavBar/NavBar.js';
-import Profile from './components/Profile/Profile.js';
-import ProgressBar from './components/ProgressBar/ProgressBar.js';
-import Quiz from './components/Quiz/Quiz.js';
+import AccessoriesFilter from './components/AccessoriesFilter.js';
+import Book from './components/Book.js';
+import BookFilter from './components/BookFilter.js';
+import CharacterBuilding from './components/CharacterBuilding.js';
+import Home from './components/Home.js';
+import Inventory from './components/Inventory.js';
+import MarketPlace from './components/MarketPlace.js';
+import NavBar from './components/NavBar.js';
+import Profile from './components/Profile.js';
+import ProgressBar from './components/ProgressBar.js';
+import Quiz from './components/Quiz.js';
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 export default function App(props) {
