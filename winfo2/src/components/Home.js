1e46890849593d139cf4f63d34388623d7ac9b92
Jenna Sorror <jsorror@uw.edu>
2024-11-01 11:43:59 -0700

    styling

--- winfo2/src/components/Home.js
+++ winfo2/src/components/Home.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
-
-export default Home
+    <div className="container">
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+    </div>
+  );
+};
+    
+export default Home;
