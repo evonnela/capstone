19b3cdca87e3524e7aa1cb56bef03fd1472cf70f
g3xo <102757915+g3xo@users.noreply.github.com>
2024-12-03 12:46:31 -0800

    fixed last name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <footer className="container-fluid">
         <p className="copyright">&copy; Level Up Learning 2024</p>
         <p className="names">
-            Created by Evonne La, Jenna Sorror, Selena Yu, Grace Kim, Niyat Efrem
+            Created by Evonne La, Jenna Sorror, Selena Yu, Grace Yim, Niyat Efrem
         </p>
         </footer>
     </div>
