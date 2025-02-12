b10f525a47576370ad5b72e9acc56be0e939b582
iamdavidxu <davidxbsxbsxbs@gmail.com>
2025-02-12 14:07:30 -0800

    david branch

--- src/components/App.js
+++ src/components/App.js
@@ -12,61 +12,85 @@ import Quiz from './Quiz.js';
 import About from './About.js';
 import Footer from './Footer.js';
 import SignInOut from './SignInOut.js';
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate,
+} from 'react-router-dom';
 import { useState } from 'react';
-import "../index.css";
+import '../index.css';
 import { pdfjs } from 'react-pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
+	'pdfjs-dist/build/pdf.worker.min.mjs',
+	import.meta.url
 ).toString();
 
 export default function App(props) {
+	const [walletPoints, setWalletPoints] = useState(0);
+	const [userId, setUserId] = useState(localStorage.getItem('user') || '');
 
-  const [walletPoints, setWalletPoints] = useState(0);
-  const [userId, setUserId] = useState(localStorage.getItem('user') || '');
+	const getCurrentUserId = () => {
+		return userId;
+	};
 
-  const getCurrentUserId = () => {
-    return userId;
-  };
-
-  return (
-    <Router basename="/WINFO2.0">
-      <div>
-        <NavBar />
-        <Routes>
-          <Route path="/AccessoriesFilter" element={<AccessoriesFilter />} />
-          <Route path="/Book" element={<Book />} />
-          <Route path="/BookFilter" element={<BookFilter />} />
-          <Route path="/CharacterBuilding" element={<CharacterBuilding walletPoints={walletPoints} setWalletPoints={setWalletPoints} userId={userId} />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/Inventory" element={<Inventory />} />
-          <Route 
-            path="/MarketPlace" 
-            element={
-              <MarketPlace 
-                userPoints={walletPoints} 
-                setUserPoints={setWalletPoints} 
-              />
-            } 
-          />
-          <Route path="/NavBar" element={<NavBar />} />
-          <Route 
-            path="/Profile" 
-            element={
-              <Profile 
-                walletPoints={walletPoints} 
-                setWalletPoints={setWalletPoints} 
-              />
-            } 
-          />
-          <Route path="/ProgressBar" element={<ProgressBar />} />
-          <Route path="/Quiz" element={<Quiz setWalletPoints={setWalletPoints} userId={getCurrentUserId()} />} />
-          <Route path="/About" element={<About />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
+	return (
+		<Router basename='/WINFO2.0'>
+			<div>
+				<NavBar />
+				<Routes>
+					<Route
+						path='/AccessoriesFilter'
+						element={<AccessoriesFilter />}
+					/>
+					<Route path='/Book' element={<Book />} />
+					<Route path='/BookFilter' element={<BookFilter />} />
+					<Route
+						path='/CharacterBuilding'
+						element={
+							<CharacterBuilding
+								walletPoints={walletPoints}
+								setWalletPoints={setWalletPoints}
+								userId={userId}
+							/>
+						}
+					/>
+					<Route path='/' element={<Home />} />
+					<Route path='/Inventory' element={<Inventory />} />
+					<Route
+						path='/MarketPlace'
+						element={
+							<MarketPlace
+								userPoints={walletPoints}
+								setUserPoints={setWalletPoints}
+							/>
+						}
+					/>
+					<Route path='/NavBar' element={<NavBar />} />
+					<Route
+						path='/Profile'
+						element={
+							<Profile
+								walletPoints={walletPoints}
+								setWalletPoints={setWalletPoints}
+							/>
+						}
+					/>
+					<Route path='/ProgressBar' element={<ProgressBar />} />
+					<Route
+						path='/Quiz'
+						element={
+							<Quiz
+								setWalletPoints={setWalletPoints}
+								userId={getCurrentUserId()}
+							/>
+						}
+					/>
+					<Route path='/About' element={<About />} />
+				</Routes>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
